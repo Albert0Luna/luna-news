3c165effdab4895e0a489afb79c46ce1c4d85780
Gustavo Alberto Robles Luna <111384757+Albert0Luna@users.noreply.github.com>
2024-08-29 00:40:21 +0000

    fix page error 1.0

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,19 +3,11 @@ import Highlights from '@/src/home/components/Highlights';
 import '@/src/home/styles/main.css';
 import '@/src/layout/styles/Footer.css';
 
-interface RootLayoutProps {
-  children: React.ReactNode
-  params: {
-    locale: 'en' | 'es'
-  }
-}
-
-export default function Page ({params: { locale }}: Readonly<RootLayoutProps>) {
-
+export default function Page ({params}: {params: {locale: string}}) {
   return (
     <main>
       <CoverPage />
-      <Highlights lang={locale}/>
+      <Highlights lang={params.locale}/>
     </main>
   );
 }
